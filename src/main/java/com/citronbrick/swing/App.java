1e628a70e801dd84545d1887aa99553652959853
CitronBrick <danielanbarasu@gmail.com>
2026-04-13 12:25:40 +0530

    demo

--- src/main/java/com/citronbrick/swing/App.java
+++ src/main/java/com/citronbrick/swing/App.java
@@ -3,6 +3,7 @@ package com.citronbrick.swing;
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
+import java.awt.event.*;
 
 /**
  * Hello world!
@@ -10,28 +11,51 @@ import java.awt.*;
 public class App {
 
 
+	private MySwing drawnPanel;
+
+
 	public void show() {
 		var frame = new JFrame("swing");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		frame.setSize(400,600);
+		frame.setSize(400,400);
 
-		var label = new JLabel("Here's a swing made with Swing");
+		var label = new JLabel("Here's a swing made with Swing", SwingConstants.CENTER);
 
 		var main = frame.getContentPane();
 		main.add(label, BorderLayout.NORTH);
 
 		var centerPanel = new JPanel();
 
-		var drawnPanel = new MySwing();
+		drawnPanel = new MySwing();
+
+
 
 		centerPanel.add(drawnPanel);
-		centerPanel.setBackground(Color.RED);
+		// centerPanel.setBackground(Color.RED);
 		main.add(centerPanel,BorderLayout.CENTER);
+		main.add(setupControls(),BorderLayout.SOUTH);
 
 		// frame.pack();
 		frame.setVisible(true);
 		frame.setLocationRelativeTo(null);
+	}
+
+	public JPanel setupControls() {
+		var panel = new JPanel();
+		// panel.setLayout(new BoxLayout(panel, BoxLayout.X_AXIS));
+		var start = new JButton("Start");
+		var stop = new JButton("Stop");
+		var faster = new JButton("Faster");
+		var slower = new JButton("Slower");
+		start.addActionListener((ActionEvent ae)->{drawnPanel.start();});
+		stop.addActionListener((ActionEvent ae)->{drawnPanel.stop();});
+		faster.addActionListener((ActionEvent ae)->{drawnPanel.faster();});
+		slower.addActionListener((ActionEvent ae)->{drawnPanel.slower();});
+		for(var b : new JButton[]{start,stop,faster,slower}) {
+			panel.add(b);
+		}
+		return panel;
 	}
 
 	
--- src/main/java/com/citronbrick/swing/MySwing.java
+++ src/main/java/com/citronbrick/swing/MySwing.java
@@ -15,12 +15,15 @@ public class MySwing extends JPanel {
 	// private double angle = -Math.PI/6;
 	private double angle = Math.PI/3;
 	private double dx = Math.PI/180;
+	private double dv = Math.PI/1800;
 	private Timer timer;
+	private ActionListener listener;
 
 	public int midX = (startX+width)/2;
 	public int midY = 20;
 
-	private int maxIterations=  3; 
+
+
 
 	private int radius = 210;
 
@@ -30,44 +33,64 @@ public class MySwing extends JPanel {
 		setBorder(new StrokeBorder(new BasicStroke(3)));
 		setPreferredSize(new Dimension(300,300));
 		var that = this;
-		final ActionListener listener = (ActionEvent ae)-> {
-			angle += Math.PI/180;
+		listener = (ActionEvent ae)-> {
+			angle += dx;
+			// dx += dv;
 			System.out.println((angle * 180)/Math.PI);
 			that.repaint();
 			if(angle > Math.PI/2) {
-				angle *= -1;
+				dx *= -1;
+				// dv *= -1;
+			} else if (angle < Math.PI/6) {
+				dx *= -1;
+				// dv *= -1;
 			}
 		};
+		this.start();
+		
+	}
+
+	public void start() {
 		SwingUtilities.invokeLater(()->{
-			var timer = new Timer(100, listener);
-			// timer.start();
+			if(timer ==null) {
+				timer = new Timer(100, listener);
+			}
+			timer.start();
 		});
+	}
+
+	public void stop() {
+		SwingUtilities.invokeLater(()->{
+			timer.stop();
+		});
+	}
+
+	public void faster() {
+		dx*=2;
+	}
+
+	public void slower() {
+		dx /= 2;
 	}
 
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		var g2 = (Graphics2D)g;
-		// var g2 = new Graphics2D();
-		g2.setStroke(new BasicStroke(7));
-		g2.setColor(Color.BLACK);
-		g2.drawLine(startX,midY, startX+width, midY);
-		// super.paintComponent(g);
+		drawBar(g2);
+		drawSeat(g2, -50);
+	}
 
-		drawSeater(g2, -50);
+	public void drawBar(Graphics2D g2) {
+		g2.setColor(Color.DARK_GRAY);
+		g2.setStroke(new BasicStroke(7));
+		g2.drawLine(startX,midY, startX+width, midY);
 	}
 
 
-	// public void drawSeater(Graphics2D g2, int delta) {
-	// 	// var sx = start
-	// 	g2.drawLine(startX , 230, endX, 230); // hori top
-	// 	g2.drawLine(endX, 230, endX+delta, 260); // verti right
-	// 	g2.drawLine(startX+delta, 260, endX+delta, 260); // hori bottom;
-	// 	g2.drawLine(startX, 230, startX+delta, 260); // verti left
-	// }
-
-	public void drawSeater(Graphics2D g2,  int delta) {
+	public void drawSeat(Graphics2D g2,  int delta) {
 		g2.setStroke(new BasicStroke(3));
+		g2.setColor(new Color(233, 116, 81));
 		var sx = midX + (int)Math.round(radius * Math.cos(angle)) - width/2;
 		var sy = midY + (int)Math.round(radius * Math.sin(angle)) - height/2 ;
 		System.out.println(sx+","+sy);
@@ -75,6 +98,17 @@ public class MySwing extends JPanel {
 		g2.drawLine(sx + width, sy , sx + width + delta, sy + height); // verti right
 		g2.drawLine(sx+delta, sy+height, sx+width+delta, sy+height); // hori bottom;
 		g2.drawLine(sx, sy, sx+delta, sy+height); // verti left
+		g2.fillPolygon(new int[]{sx,sx + width,sx + width+delta,sx+delta }, new int[]{sy, sy, sy+height,sy+height  }, 4);
+
+
+		//draw chains
+		var dashArray = new float[]{3};
+		g2.setStroke(new BasicStroke(2,BasicStroke.CAP_ROUND,BasicStroke.JOIN_ROUND, 1f,dashArray , 1f));
+		g2.setColor(Color.DARK_GRAY);
+		g2.drawLine(midX,midY, sx, sy);
+		g2.drawLine(midX+width,midY, sx+width, sy);
+		g2.drawLine(midX+width,midY, sx+width+delta, sy+height);
+		g2.drawLine(midX,midY, sx+delta, sy+height);
 	}
 
 
